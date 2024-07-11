472e3fcb4f98508052ac930ddb37241debae88b9
hana-week <fairy9894@gmail.com>
2024-07-11 23:51:45 +0900

    [Bronze II] Title: 문자열 반복, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.py
@@ -0,0 +1,13 @@
+a = int(input())
+
+for i in range(a):
+    b = list(input().split())
+    c = int(b[0])
+    d = list(b[1])
+    
+    
+            
+    for i in range(len(b[1])):
+        for j in range(c):
+            print(d[i], end="")
+    print('')
