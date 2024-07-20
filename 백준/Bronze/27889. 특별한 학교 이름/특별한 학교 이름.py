dc68550cbf6524116b651f745fbc2dcbe6308b41
hana-week <fairy9894@gmail.com>
2024-07-20 21:15:29 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.py
@@ -0,0 +1,14 @@
+# NLCS: North London Collegiate School
+# BHA: Branksome Hall Asia
+# KIS: Korea International School
+# SJA: St. Johnsbury Academy
+
+x=input()
+if x=='NLCS':
+    print('North London Collegiate School')
+elif x=='BHA':
+    print('Branksome Hall Asia')
+elif x=='KIS':
+    print('Korea International School')
+elif x=='SJA':
+    print('St. Johnsbury Academy')
