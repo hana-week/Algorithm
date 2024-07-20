56221cf7b53f010b627ed14597aa12fe4326e893
hana-week <fairy9894@gmail.com>
2024-07-20 21:21:28 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.py
@@ -0,0 +1,14 @@
+# 구호 SONGDO에 대해 HIGHSCHOOL로 응원.
+# 구호 CODE에 대해 MASTER로 응원.
+# 구호 2023에 대해 0611로 응원.
+# 구호 ALGORITHM에 대해 CONTEST로 응원.
+
+x=input()
+if x=='SONGDO':
+    print('HIGHSCHOOL')
+elif x=='CODE':
+    print('MASTER')
+elif x=='2023':
+    print('0611')
+elif x=='ALGORITHM':
+    print('CONTEST')
